d839ec127fc7eef2f353193a1f945e38a10b374e
mainmachine <dave@b-sharpmusic.com>
2015-06-09 11:28:23 -0400

    - added autostart "Load LADSPA.desktop" entry - modified usr/bin/load-ladspa script to source etc/default/pulsecomp for DEFAULT_SINK - updated install script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 sudo cp src/etc/default/pulsecomp /etc/default/pulsecomp
-sudo cp src/lib/systemd/system/pulsecomp.service /lib/systemd/system/pulsecomp.service
 sudo cp src/usr/bin/load-ladspa /usr/bin/load-ladspa
-sudo systemctl start pulsecomp
-sudo systemctl enable pulsecomp
+sudo cp src/Load\ LADSPA.desktop /home/$USER/.config/autostart/Load\ LADSPA
+/usr/bin/load-ladspa &
