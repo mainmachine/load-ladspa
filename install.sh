d3f8b3bd5b7947f04a1cd159bab16614bceb21d5
mainmachine <dave@b-sharpmusic.com>
2015-06-09 12:00:03 -0400

    - updated install.sh to reflect path changes for "Load LADSPA.desktop"

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo cp src/etc/default/pulsecomp /etc/default/pulsecomp
 sudo cp src/usr/bin/load-ladspa /usr/bin/load-ladspa
-sudo cp src/Load\ LADSPA.desktop /home/$USER/.config/autostart/Load\ LADSPA
+sudo cp src/.config/autostart/Load\ LADSPA /home/$USER/.config/autostart/Load\ LADSPA
 /usr/bin/load-ladspa &
