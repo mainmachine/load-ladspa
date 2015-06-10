d73828db7dc369a696026f3b346fd88f533a31a9
David Martinka <dave@b-sharpmusic.com>
2015-06-10 12:12:08 -0400

    - fixed install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo cp src/etc/default/pulsecomp /etc/default/pulsecomp
 sudo cp src/usr/bin/load-ladspa /usr/bin/load-ladspa
-sudo cp src/.config/autostart/Load\ LADSPA /home/$USER/.config/autostart/Load\ LADSPA
+sudo cp src/.config/autostart/Load\ LADSPA.desktop /home/$USER/.config/autostart/Load\ LADSPA.desktop
 /usr/bin/load-ladspa &
